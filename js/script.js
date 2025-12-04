673ef6e10c157c96d5f075c2a67aadf396a2a367
Fábio Rocha <fabiorocha@Fabios-MacBook-Pro.local>
2025-12-04 15:20:33 +0000

    5ª tarefa cumprida

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
 //ficheiro de script
+addEventListener("DOMContentLoaded", () => {
+  (() => {
+    "use strict";
+
+    const forms = document.querySelectorAll(".needs-validation");
+
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+
+          form.classList.add("was-validated");
+        },
+        false
+      );
+    });
+  })();
+});
